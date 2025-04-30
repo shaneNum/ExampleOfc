dbbcecb906c6759822238ea3e2cc6b413fdb2cb9
ShaneNum <150904595+shaneNum@users.noreply.github.com>
2025-04-30 20:52:11 +0700

    Update TheFineExample.c
    Add 2 more boxes!

--- TheFineExample.c
+++ TheFineExample.c
@@ -10,26 +10,26 @@ int main() {
 	printf("  |____|   |___|  /\\___  > /_______  /__/\\_ \\(____  /__|_|  /   __/|____/\\___  >\n");
 	printf("                \\/     \\/          \\/      \\/     \\/      \\/|__|             \\/ \n");
 	
-	
-	printf("\n\n                                  \\O/\n");
-	for(int i = 0; i < 5; i++ ){
-		
-		for(int j = 0; j < 5; j++ ){
-			printf("      ");
-		}
-		
-		for(int j = 0; j < 5; j++ ){
-			if(i != 2 || j != 2){
-				if(i != 2 || j != 3){
-					printf(" ");
-				}
-					printf("#");
-			}else{
-				printf("BRO");
+	for(int k = 0; k < 2; k++ ){
+		printf("\n\n                                  \\O/\n");
+		for(int i = 0; i < 5; i++ ){
+			
+			for(int j = 0; j < 5; j++ ){
+				printf("      ");
 			}
+			
+			for(int j = 0; j < 5; j++ ){
+				if(i != 2 || j != 2){
+					if(i != 2 || j != 3){
+						printf(" ");
+					}
+						printf("#");
+				}else{
+					printf("BRO");
+				}
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
-	
 	return 0;
 }
